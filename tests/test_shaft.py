de1baab2c495514c0dcb6193e2498b3d4d74e714
Gianmarco Gurioli <ggurioli@sissa.it>
2021-05-17 15:46:36 +0200

    Minor revision of the Shaft class

--- tests/test_shaft.py
+++ tests/test_shaft.py
@@ -8,11 +8,20 @@ class TestShaft(TestCase):
     Test case for the Shaft class.
     """
 
-    def test_generate_solid(self):
+    def test_generate_solid_01(self):
         sh = Shaft("tests/test_datasets/shaft.iges")
         shaft_solid = sh.generate_solid()
         self.assertIsInstance(shaft_solid, TopoDS_Solid)
 
-    def test_display(self):
+    def test_generate_solid_02(self):
+        sh = Shaft("tests/test_datasets/shaft.stl")
+        shaft_solid = sh.generate_solid()
+        self.assertIsInstance(shaft_solid, TopoDS_Solid)
+
+    def test_display_01(self):
         sh = Shaft("tests/test_datasets/shaft.iges")
+        sh.display()
+
+    def test_display_02(self):
+        sh = Shaft("tests/test_datasets/shaft.stl")
         sh.display()
