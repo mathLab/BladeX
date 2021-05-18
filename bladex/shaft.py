ea5206b515008c5f45dd2e6cf37b8c517c2f8d49
Gianmarco Gurioli <ggurioli@sissa.it>
2021-05-18 13:37:58 +0200

    shaft.py: raise Exception on the shaft file format included

--- bladex/shaft.py
+++ bladex/shaft.py
@@ -29,13 +29,15 @@ class Shaft(object):
         :rtype: OCC.Core.TopoDS.TopoDS_Solid
         """
         ext = os.path.splitext(self.filename)[1][1:]
-        if ext=='stl':
+        if ext == 'stl':
         	shaft_compound = read_stl_file(self.filename)
-        elif ext=='iges':
+        elif ext == 'iges':
         	iges_reader = IGESControl_Reader()
         	iges_reader.ReadFile(self.filename)
         	iges_reader.TransferRoots()
         	shaft_compound = iges_reader.Shape()
+        else:
+        	raise Exception('The shaft file is not in iges/stl formats')
         sewer = BRepBuilderAPI_Sewing(1e-2)
         sewer.Add(shaft_compound)
         sewer.Perform()
