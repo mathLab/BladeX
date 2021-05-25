29ab32d1dca607cd18a10b7fe864fb515436441c
Gianmarco Gurioli <ggurioli@sissa.it>
2021-05-25 09:58:39 +0200

    Removing the display test from tests

--- tests/test_shaft.py
+++ tests/test_shaft.py
@@ -18,14 +18,6 @@ class TestShaft(TestCase):
         shaft_solid = sh.generate_solid()
         self.assertIsInstance(shaft_solid, TopoDS_Solid)
 
-    def test_display_01(self):
-        sh = Shaft("tests/test_datasets/shaft.iges")
-        sh.display()
-
-    def test_display_02(self):
-        sh = Shaft("tests/test_datasets/shaft.stl")
-        sh.display()
-
     def test_exception(self):
     	sh = Shaft("tests/test_datasets/parameters.prm")
     	with self.assertRaises(Exception):
