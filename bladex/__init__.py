dc30c8383a44a86b217a103bd441369252655206
aivagnes <aivagnes@sissa.it>
2021-10-14 18:08:31 +0200

    insert reverse propeller

--- bladex/__init__.py
+++ bladex/__init__.py
@@ -1,12 +1,13 @@
 """
 BladeX init
 """
-__all__ = ['profilebase', 'nacaprofile', 'customprofile', 'blade', 'shaft', 'propeller', 'deform', 'params', 'ndinterpolator']
+__all__ = ['profilebase', 'nacaprofile', 'customprofile', 'reversepropeller', 'blade', 'shaft', 'propeller', 'deform', 'params', 'ndinterpolator']
 
 from .meta import *
 from .profilebase import ProfileBase
 from .nacaprofile import NacaProfile
 from .customprofile import CustomProfile
+from .reversepropeller import ReversePropeller
 from .blade import Blade
 from .shaft import Shaft
 from .propeller import Propeller
